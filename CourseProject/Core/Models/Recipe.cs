ba7970d15f11068ee41ec7ddd16182a44ac3f9d7
Julia Pavlenko <yuliapavlenko2403@gmail.com>
2025-02-28 21:00:13 +0200

    Creating classes for entities

---
+++ CourseProject/Core/Models/Recipe.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class Recipe
+    {
+        private string _title;
+        private List<string> _ingredients;
+        private string _instructions;
+
+        public string Title 
+        {   
+            get { return _title; } 
+            set { _title = value; } 
+        }
+        public List<string> Ingredients 
+        { 
+            get { return new List<string>(_ingredients); } 
+            set { _ingredients = new List<string>(value); } 
+        }
+        public string Instructions 
+        { 
+            get { return _instructions; } 
+            set { _instructions = value; } 
+        }
+    }
+}
---
+++ CourseProject/Core/Models/RecipeBook.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class RecipeBook
+    {
+        private string _title;
+        private string _author;
+        private string _description;
+        private Recipe[] _recipes;
+
+        public string Title 
+        { 
+            get { return _title; } 
+            set { string _title; } 
+        }
+        public string Author 
+        { 
+            get { return _author; } 
+            set { _author = value; } 
+        }
+        public string Description 
+        { 
+            get { return _description; } 
+            set { _description = value; } 
+        }
+        public Recipe[] Recipes 
+        { 
+            get { return _recipes; } 
+            set { _recipes = value; } 
+        }    
+    }
+}
