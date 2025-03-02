55624f75d900c0aba096eeb5ce12fff60c36f9b9
Julia Pavlenko <yuliapavlenko2403@gmail.com>
2025-03-02 23:25:08 +0200

    Creating interfaces for key functionality

---
+++ CourseProject/Application/Abstractions/IRecipeAndBookService.cs
@@ -0,0 +1,16 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Abstractions
+{
+    public interface IRecipeAndBookService
+    {
+        void Publish();
+        void View();
+        void Read();
+    }
+}
