7f24d76a89d1f05750efdc67d79aa5057bbb2e89
Julia Pavlenko <yuliapavlenko2403@gmail.com>
2025-03-02 23:25:08 +0200

    Creating interfaces for key functionality

---
+++ CourseProject/Application/Abstractions/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Abstractions
+{
+    public interface IUserService
+    {
+        void Register(string username, string password);
+        void Login(string username, string password);
+        void Logout();
+        void ChangeUsername();
+        void ChangePassword(string password);
+        void DeleteAccount();
+    }
+}
