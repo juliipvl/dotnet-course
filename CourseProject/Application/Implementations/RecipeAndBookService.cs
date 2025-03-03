86999ac2550673aa5fcf0542f34c2998c0cbf5ff
Julia Pavlenko <yuliapavlenko2403@gmail.com>
2025-03-03 17:10:00 +0200

    Creating classes that inherit from interfaces

---
+++ CourseProject/Application/Implementations/RecipeAndBookService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Implementations
+{
+    public class Recipe : Application.Abstractions.IRecipeAndBookService
+    {
+        public void Publish()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void View()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Book : Application.Abstractions.IRecipeAndBookService
+    {
+        public void Publish()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void View()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CourseProject/Application/Implementations/UserService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Implementations
+{
+    public class UserService : Application.Abstractions.IUserService
+    {
+        public void ChangePassword(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeUsername()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAccount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
