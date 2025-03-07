2748696f0026d11643a3dd8e050e4d835f2728a9
Julia Pavlenko <yuliapavlenko2403@gmail.com>
2025-03-07 13:31:06 +0200

    Implementing a Custom List<T>

---
+++ CourseProject/CourseProject/CustomList.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProject
+{
+    public class CustomList<T> : IEnumerable<T>
+    {
+        private T[] _items;
+        private int _size;
+        private int _capacity;
+
+        public int Count => _size;
+        public int Capacity => _capacity;
+
+        public CustomList()
+        {
+            _size = 0;
+            _capacity = 5;
+            _items = new T[_capacity];
+        }
+
+        public CustomList(int capacity)
+        {
+            _size = 0;
+            _capacity = capacity;
+            _items = new T[_capacity];
+        }
+
+        public CustomList(T[] items)
+        {
+            _size = items.Length;
+            _capacity = items.Length;
+            _items = new T[_capacity];
+            Array.Copy(items, _items, _size);
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= _size)
+                    throw new IndexOutOfRangeException();
+                return _items[index];
+            }
+            set
+            {
+                if (index < 0 || index >= _size)
+                    throw new IndexOutOfRangeException();
+                _items[index] = value;
+            }
+        }
+
+        public void Add(T newItem)
+        {
+            if (_size == _capacity)
+            {
+                Resize(_capacity * 2);
+            }
+            _items[_size++] = newItem;
+        }
+
+        public void Remove(T item)
+        {
+            int index = Array.IndexOf(_items, item, 0, _size);
+            if (index != -1)
+            {
+                RemoveAt(index);
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= _size)
+                throw new ArgumentOutOfRangeException(nameof(index), "Index is out of range.");
+
+            for (int i = index; i < _size - 1; i++)
+            {
+                _items[i] = _items[i + 1];
+            }
+            _items[_size - 1] = default(T);
+            _size--;
+        }
+
+        private void Resize(int newCapacity)
+        {
+            T[] newItems = new T[newCapacity];
+            Array.Copy(_items, newItems, _size);
+            _items = newItems;
+            _capacity = newCapacity;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                yield return _items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+}
--- CourseProject/CourseProject/Program.cs
+++ CourseProject/CourseProject/Program.cs
@@ -4,12 +4,16 @@ namespace CourseProject
 {
     internal class Program
     {
+        /*
         class Cat
         {
             public string Name;
         } 
+        */
+
         static void Main(string[] args)
         {
+            /*
             // Value Type
             Console.WriteLine("VALUE TYPE");
             int firstVal = 5;
@@ -25,7 +29,7 @@ namespace CourseProject
             // Reference Type
             Console.WriteLine("\nREFERENCE TYPE");
 
-            Cat cat1 = new Cat() { Name = "Sam" } ;
+            Cat cat1 = new Cat() { Name = "Sam" };
             Console.WriteLine($"Name of first cat: {cat1.Name}");
 
             Cat cat2 = cat1;
@@ -36,6 +40,41 @@ namespace CourseProject
             Console.WriteLine("\n---After changing the value in the second cat---");
             Console.WriteLine($"Second cat will change name: {cat2.Name}");
             Console.WriteLine($"Both cats point to the same address, so the name of the first one will also change: {cat1.Name}");
+            */
+
+        CustomList<int> list = new CustomList<int>();
+            Console.WriteLine($"Initial Capacity: {list.Capacity}");
+            Console.WriteLine($"Initial Count: {list.Count}");
+
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(6); 
+
+            Console.WriteLine($"Count after adding elements: {list.Count}");
+            Console.WriteLine($"Capacity after resize: {list.Capacity}");
+
+            Console.WriteLine($"Element at index 2: {list[2]}");
+            list[2] = 10;
+            Console.WriteLine($"New element at index 2: {list[2]}");
+
+            list.Remove(10);
+            Console.WriteLine($"Count after removing element: {list.Count}");
+
+            list.RemoveAt(1);
+            Console.WriteLine($"Count after removing at index 1: {list.Count}");
+
+            int[] arr = { 100, 200, 300 };
+            CustomList<int> arrayList = new CustomList<int>(arr);
+            Console.WriteLine($"Array constructor count: {arrayList.Count}");
+
+            Console.WriteLine("Elements in the list:");
+            foreach (int item in list)
+            {
+                Console.WriteLine(item);
+            }
 
         }
     }
