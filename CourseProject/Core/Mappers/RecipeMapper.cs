0019ba266fe3a1a238424fc1de3b7b9e9945b2a5
Julia Pavlenko <yuliapavlenko2403@gmail.com>
2025-04-13 23:49:41 +0300

    create custom mapping

---
+++ CourseProject/Core/Mappers/RecipeMapper.cs
@@ -0,0 +1,30 @@
+﻿using Core.DTOs;
+using Core.Models;
+
+namespace Core.Mappers
+{
+    public static class RecipeMapper
+    {
+        public static RecipeDto MapToDto(this Recipe recipe)
+        {
+            return new RecipeDto
+            {
+                Id = recipe.Id,
+                Title = recipe.Title,
+                Ingredients = recipe.Ingredients.Select(IngredientMapper.MapToDto).ToList(),
+                Instructions = recipe.Instructions.Select(InstructionMapper.MapToDto).ToList()
+            };
+        }
+
+        public static Recipe MapToModel(this RecipeDto dto)
+        {
+            return new Recipe
+            {
+                Id = dto.Id,
+                Title = dto.Title,
+                Ingredients = dto.Ingredients.Select(IngredientMapper.MapToModel).ToList(),
+                Instructions = dto.Instructions.Select(InstructionMapper.MapToModel).ToList()
+            };
+        }
+    }
+}
